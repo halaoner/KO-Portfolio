5e4e65c2f3520c4e0ca9c0ffa44733d127c24d0b
Ondrej Halaska <halaska.o@seznam.cz>
2022-02-12 20:28:26 +0100

    Fix compare function

--- app/dynamoDB/dynamodbRead.js
+++ app/dynamoDB/dynamodbRead.js
@@ -1,5 +1,6 @@
 const AWS = require("aws-sdk");
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
+const { use } = require("../routes/admin");
 
 AWS.config.update({
     region: "eu-west-1",
@@ -8,60 +9,71 @@ AWS.config.update({
 
 const docClient = new AWS.DynamoDB.DocumentClient();
 
+async function readData(username) {
+    const params = {
+        TableName: "Users",
+        ProjectionExpression: "username, password",
+        KeyConditionExpression: "username = :username",
+        ExpressionAttributeValues: {
+            ":username": username,
+            // ":password": savedPassword
+        }
+    }
+    try {
+        const data = await docClient.query(params).promise()
+        if (data.Count > 0) {
+            // console.log("Query succeeded.");
+            data.Items.forEach(function (item) {
+                console.log("username:", item.username + "; " + "hashedPassword:", item.password)
+                savedUsername = item.username
+                savedPassword = item.password
+                console.log('Reading from DB: data found!')
+                // return savedPassword && savedUsername
+                //----- 'savedPassword' is the result of the if condition -----//
+                return savedPassword
+            })
+        }
+        else {
+            console.log('Reading from DB: Username or hash DO NOT MATCH with the stored one!')
+        }
+        // console.log('DATA --->', savedPassword)
+
+        //----- 'savedPwd' is the result of the readData() function
+        return savedPwd
+    }
+    catch (err) {
+        // console.log("Error:", err);
+        console.log("Unable to query. Error:", JSON.stringify(err, null, 2));
+    }
+}
+
+
+//-------- compare 'password' provided by the user with 'savedPassword' stored in DB --------//
 const comparePwd = async (password, savedPassword) => {
-    //-------- read data from DB --------//
-    async function readData(username, savedPassword) {
-        const params = {
-            TableName: "Users",
-            ProjectionExpression: "username, password",
-            KeyConditionExpression: "password = :password and username = :username",
-            ExpressionAttributeValues: {
-                ":username": username,
-                ":password": savedPassword
-            }
-        }
-        try {
-            const data = await docClient.query(params).promise()
-            if (data.Count > 0) {
-                // console.log("Query succeeded.");
-                data.Items.forEach(function (item) {
-                    console.log("username:", item.username + "; " + "hashedPassword:", item.password)
-                    savedUsername = item.username
-                    savedPassword = item.password
-                    console.log('Reading from DB: data found!')
-                    return savedPassword && savedUsername
-                })
-            }
-            else {
-                console.log('Reading from DB: Username DOES NOT match with the stored one!')
-            }
-        }
-        catch (err) {
-            // console.log("Error:", err);
-            console.log("Unable to query. Error:", JSON.stringify(err, null, 2));
-        }
-    };
-
-    //-------- read data from DB --------//
-    const username = 'Kate'
-    readData(username, savedPassword)
-
-    //-------- compare 'password' provided by the user with 'savedPassword' stored in DB --------//
     const result = await bcrypt.compare(password, savedPassword)
     if (result) {
         console.log('HASH MATCH! Successfully logged in!')
     } else {
         console.log('HASH NOT MATCHED. Try it again!')
     }
-    return result
-}
+};
 
-// TODO: test the functionality properly
-async function compareHash() {
-    const password = 'admin'
-    const savedPassword = '$2b$10$HGvd37u6MEfO6OySnGodOe4HUzbF6eiKdbGF6dDh/.L8l.Z8gaJJ6'
+async function compare() {
+    const username = 'Kate'
+    const password = 'admin12'
+    // const read = await readData(username, savedPassword)
+    await readData(username)
+    console.log('Read Data:', savedPassword)
     await comparePwd(password, savedPassword)
-    console.log('Data quering done!')
 }
 
-compareHash()
+compare()
+
+//TODO: username and hash must match --> return savedPassword & savedUsername
+
+
+// ------- username, password and hash must match to get following output:
+// username: Kate; hashedPassword: $2b$10$mcaXb7BUH3hRKfkMSmhc1uoBXFH4C8iZzcU/MLxqevzpPrUZPSBri
+// Reading from DB: data found!
+// HASH MATCH! Successfully logged in!
+// Data quering done!
